89506d7e579bc9abfef8fa37570a851ec53b0fcf
Abraha Kahsay <abrahak81@gmail.com>
2022-09-13 01:09:11 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -5,7 +5,7 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu','2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon','2017-05-12', 5, TRUE, 11.0);
 
--- Update for type error in date of birth for row id=2
+-- Update for typing error in date of birth for row id=2
 UPDATE animals
 SET date_of_birth = '2018-11-15'
 WHERE ID = 2;
