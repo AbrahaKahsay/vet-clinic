312c8aacbc97627f4f4a67ed662a357b307a1a96
Abraha Kahsay <abrahak81@gmail.com>
2022-09-14 23:27:23 +0300

    Finish work

--- queries.sql
+++ queries.sql
@@ -1,16 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-30';
-SELECT name FROM animals WHERE neutered='t' AND escape_attempts<3;
-SELECT date_of_birth FROM animals WHERE name IN ('Augmon', 'Pikachu');
-SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
-SELECT * FROM animals WHERE neutered='t';
-SELECT * FROM animals WHERE NOT name='Gabumon';
-SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <=17.3;
-
--- New data entry
-
 -- Inside a transaction update the animals table by setting the species column to unspecified.Then roll back the change
 -- and verify that the species columns went back to the state before the transaction.
 BEGIN;
@@ -64,3 +52,7 @@ SELECT MAX(weight_kg) FROM animals;
 --Who escapes the most, neutered or not neutered animals?
 SELECT neutered, SUM(escape_attempts) FROM animals
 GROUP BY neutered;
+
+SELECT date_of_birth, AVG(escape_attempts) FROM animals
+GROUP BY date_of_birth
+HAVING date_of_birth > '1990-01-01' AND date_of_birth < '2000-12-30';
