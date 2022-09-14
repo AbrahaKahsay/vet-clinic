3dd29b7dd9ad7a2ea7932694a09bc790291a2ef4
Abraha Kahsay <abrahak81@gmail.com>
2022-09-14 23:28:39 +0300

    Remove previous data

--- data.sql
+++ data.sql
@@ -1,15 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon','2020-02-03', 0, TRUE, 10.23);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon','2021-11-15', 2, TRUE, 8.0);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu','2021-01-07', 1, FALSE, 15.04);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon','2017-05-12', 5, TRUE, 11.0);
-
--- Update for typing error in date of birth for row id=2
-UPDATE animals
-SET date_of_birth = '2018-11-15'
-WHERE ID = 2;
-
 -- Delete all the existing data from animals table
 BEGIN;
 DELETE FROM animals;
