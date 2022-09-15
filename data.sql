6c33c7e957786464db15a4f8db5fe66b1423d1b8
Abraha Kahsay <abrahak81@gmail.com>
2022-09-15 18:45:13 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -34,5 +34,5 @@ UPDATE animals SET species_id = 2 WHERE name NOT LIKE '%mon';
 UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
 UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
 UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
-UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmandar', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
