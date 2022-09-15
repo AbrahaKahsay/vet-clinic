2e4014ac45a832daefc73f802b6b9b613673590b
Abraha Kahsay <abrahak81@gmail.com>
2022-09-15 15:43:53 +0300

    Update schema with foreign keys

--- schema.sql
+++ schema.sql
@@ -32,10 +32,15 @@ CREATE TABLE species (
 ALTER TABLE animals
 DROP COLUMN species;
 
+-- Add species_id and owner_id to animals table
 ALTER TABLE animals
-ADD COLUMN species_id INT
-REFERENCES species;
+ADD species_id INT;
 
 ALTER TABLE animals
-ADD COLUMN owner_id INT
-REFERENCES owners;
+ADD owner_id INT;
+
+-- Add foreign key constraints to aanimals table by referencing the species and owners table
+ALTER TABLE animals
+ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species (id);
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners (id);
