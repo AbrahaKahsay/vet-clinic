51fb4ea8cb85a5218cd5540152a0d4290c213243
Abraha Kahsay <abrahak81@gmail.com>
2022-09-19 23:33:30 +0300

    Add data to visits and owners

--- schema.sql
+++ schema.sql
@@ -70,3 +70,10 @@ CREATE TABLE visits (
     CONSTRAINT fk_animals FOREIGN KEY (animal_id) REFERENCES animals (id),
     CONSTRAINT fk_vets FOREIGN KEY (vet_id) REFERENCES vets (id)
     );
+
+INSERT INTO visits (animal_id, vet_id, visit_date) 
+SELECT * FROM (SELECT id FROM animals) animal_id, 
+(SELECT id FROM vets) vet_id, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+insert into owners (full_name, email) 
+select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
